de8e80e41236e6d8ae52c51e4f91b48c6bd65465
Ali Aftab Muhammad <37605427+Renfrew100@users.noreply.github.com>
2018-03-29 14:39:30 -0400

    Update SquareBasedPyramid.java

--- SquareBasedPyramid.java
+++ SquareBasedPyramid.java
@@ -1,19 +1,18 @@
 package square;
-public class RectangularPrism extends main {
+public class SquareBasedPyramid extends main {
     public double height;
     
-    public RectangularPrism(double length, double width, double height) {
+    public SquareBasedPyramid(double length, double width, double height) {
         super(length, width);
         this.height = height;
     }
     
-    public double Volume_R (){
-        return length1*length1*height;     
+    public double Volume_S (){
+        return (length1*length1*height)/3;     
     }
-    public double SA_R(){
-        return Math.pow(length1,2)*2 + 4*length1*height;
+    public double SA_S(){
+        return Math.pow(length1,2) + 2*length1*(Math.sqrt(Math.pow(length1,2)/4 + Math.pow(height,2)));
     } 
-   
+  
+    
 }
-
-
