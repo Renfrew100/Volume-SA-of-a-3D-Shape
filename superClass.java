327792a95d5b39ebb43b250965fe948b5268aa48
Ali Aftab Muhammad <37605427+Renfrew100@users.noreply.github.com>
2018-03-29 13:55:59 -0400

    Update superClass.java

--- superClass.java
+++ superClass.java
@@ -6,25 +6,9 @@ public class main {
     public double length1;
     public double width1;
     
-    public main (double length, double width){
-        length1 = length;
-        width1 = width;
-    }
-    
-    public double square_Length (){
-        return length1;
-    }
-package square;
-import java.util.Scanner;
-
-public class main {
-    
-    public double length1;
-    public double width1;
-    
-    public main (double length, double width){
-        length1 = length;
-        width1 = width;
+    public main (double length1, double width1){
+       this.length1 = length1;
+       this.width1 = width1;
     }
     
     public double square_Length (){
@@ -42,31 +26,37 @@ public class main {
     public static void main(String[] args) {
     
     while(true) { 
+        
+        //User inputs length to be used for all classes
         System.out.println("");
-        System.out.println("Enter a length to be used: ");
+        System.out.print("Enter a length to be used: ");
         Scanner input = new Scanner(System.in);
         double length = input.nextDouble();
         /*System.out.println("The length entered is " + length + "and the width is " + length);
         main newSquare = new main(length,length);
         System.out.println("The area of the square is:  " + newSquare.area());*/
         
-    //User inputs height to be used for a cube
-        System.out.println("Enter a height to be used: ");
+        //User inputs height to be used for all classes 
+        System.out.print("Enter a height to be used: ");
         double height = input.nextDouble();
-        System.out.println("The height entered is " + height);
-        Cube newCube = new Cube(length, length, height);
-        System.out.println("The volume of the cube is " + newCube.Volume_C());
-        System.out.println("The SA of the cube is " + newCube.SA_C());
-    
-     //An object was created for the rectangular prism  
-        RectangularPrism newPrism = new RectangularPrism(length, length, height);
-        System.out.println("The volume of the rectangular prism is " + newPrism.Volume_R());
-        System.out.println("The SA of the rectangular prism is " + newPrism.SA_R());
+        System.out.println("");
         
-    //An object was created for the square based pyramid
-        Square_Based_Pyramid newPyramid = new Square_Based_Pyramid(length,length,height);
-        System.out.println("The volume of the square based pyramid is " + newPyramid.Volume_P());
-        System.out.println("The SA of the square based pyramid is " + newPyramid.SA_P());
+        //An object was created for the cube 
+        Cube newCube = new Cube(length, length, height);
+        System.out.println("The Volume of the Cube is " + newCube.Volume_C());
+        System.out.println("The Surface Area of the Cube is " + newCube.SA_C());
+        System.out.println("");
+    
+        //An object was created for the rectangular prism  
+        RectangularPrism newPrism = new RectangularPrism(length, length, height);
+        System.out.println("The Volume of the Rectangular Prism is " + newPrism.Volume_R());
+        System.out.println("The Surface Area of the Rectangular Prism is " + newPrism.SA_R());
+        System.out.println("");
+        
+        //An object was created for the square based pyramid
+        SquareBasedPyramid newPyramid = new SquareBasedPyramid(length,length,height);
+        System.out.println("The Volume of the Square-Based Pyramid is " + newPyramid.Volume_S());
+        System.out.println("The Surface Area of the Square-Based Pyramid is " + newPyramid.SA_S());
         
     }
     }
